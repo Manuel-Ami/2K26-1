650f25b374c5527e5b93b0c74dc79678b5e105fd
Manuel-Ami <amissahemma241@gmail.com>
2026-01-24 22:37:35 -0900

    introductory ts

--- test.js
+++ test.js
@@ -1,27 +1,19 @@
-type Roletype = ("admin" | "read" | "read-write" | "readOnly");
-
-function assignRoles(role:Roletype) {
+function assignRoles(role) {
     switch (role) {
         case "admin":
             console.log("Fam is assigned an admin role");
             break;
-
         case "read":
             console.log("Nigga is assigned an read role");
             break;
-
         case "read-write":
             console.log("Yn is assigned an read-write role");
             break;
-
         case "readOnly":
             console.log("Blud assigned an readOnly role");
             break;
-
         default:
             break;
     }
 }
-
-
 assignRoles("admin");
