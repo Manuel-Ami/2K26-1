95f90a99ba74baf918c8ea1c608726e81c4a1a83
Manuel-Ami <amissahemma241@gmail.com>
2026-02-01 17:50:09 -0900

    introductory ts

--- test1.ts
+++
@@ -1,9 +0,0 @@
-const people = [
-  { name: "Alice", age: 30 },
-  { name: "Bob", age: 25 },
-  { name: "Charlie", age: 35 },
-];
-
-
-const names = people.map((person) => person.name);console.log(names);
-console.log(names);
