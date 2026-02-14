48f057bc582ea7abbebfe53ad493f1d2cbe955e0
Manuel-Ami <amissahemma241@gmail.com>
2026-02-13 16:23:06 -0900

    introductory ts

--- test1.ts
+++ test1.ts
@@ -1,10 +1,3 @@
-function add(a: number, b: number): number {
-    return a + b;
-}
-
-console.log(add(5, 3));
-
-
 function Obliterate(person : string): void {
     console.log(`Hello  ${person}..., You have been selected for obliteration!\nDon't worry, it's just a simulation.`);
     
