2359a923cc2d4e8f7d78f5ace82bd5735c74fdf0
Manuel-Ami <amissahemma241@gmail.com>
2026-02-16 14:00:00 +0100

    intro ts

--- test1.ts
+++ test1.ts
@@ -1,12 +1,6 @@
-function Obliterate(person : string): void {
-    console.log(`Hello  ${person}..., You have been selected for obliteration!\nDon't worry, it's just a simulation.`);
-    
-}
-
 let con : boolean = true;
-
 
 if (con) {
     console.log("Yes yes we do want to obliterate you\nStarting the obliteration process...");
-    Obliterate("John Doe");
+    
 }
