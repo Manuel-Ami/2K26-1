f75df713e68ac205d94030babab7ae167373d4ba
Manuel-Ami <amissahemma241@gmail.com>
2026-02-15 15:03:57 -0900

    intro ts

--- test1.js
+++
@@ -1,8 +0,0 @@
-function Obliterate(person) {
-    console.log("Hello  ".concat(person, "..., You have been selected for obliteration!\nDon't worry, it's just a simulation."));
-}
-var con = true;
-if (con) {
-    console.log("Yes yes we do want to obliterate you\nStarting the obliteration process...");
-    Obliterate("John Doe");
-}
