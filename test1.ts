17146af8368c620ff1a7cc60556b94b2ec6c203f
Manuel-Ami <amissahemma241@gmail.com>
2026-02-25 14:00:00 +0100

    intro ts

--- test1.ts
+++ test1.ts
@@ -2,9 +2,4 @@ function invent(object:string):void {
 
     console.log(`Object Processing.....\nInventing ${object}`); 
 }
-
-
-
-
-console.log("into the santi mo");
-console.log("Me amor de lanus inspirus")
+invent("Car");
