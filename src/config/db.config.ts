0989bd56a334b8689f5e06bc92cf2fe19bd53b68
Manuel-Ami <amissahemma241@gmail.com>
2026-04-02 18:11:14 -0900

    ts config

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,2 +1,6 @@
-import {  } from " ";
-import dotenv
+import dotnenv from 'dotenv';
+dotnenv.config();
+
+export const dbConfig = {
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '3306', 10),
